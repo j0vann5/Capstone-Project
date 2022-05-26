8aa88fe814b90e78e26c284ced2180af5d10ed4a
j0vann5 <102550754+j0vann5@users.noreply.github.com>
2022-05-26 03:05:48 -0400

    I made the pictures buttons and resized them according

---
+++ Views/Home/DietFinalCalculations.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Capstone_Project.Models;
+
+namespace CalCalculator.Views.Home
+{
+    
+
+
+    public class DietFinalCalculationsModel : PageModel
+    {
+        
+        public void OnGet()
+        {
+        
+    }
+    }
+}
